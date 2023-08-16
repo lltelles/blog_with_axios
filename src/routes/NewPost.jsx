4d9f0b14ac9ca2a4362d7bcb6e178ed9c69870a0
Lucas Telles <lltellesss@gmail.com>
2023-08-16 18:46:47 -0300

    NewPost page axios post method working

--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -1,28 +1,54 @@
+/* eslint-disable no-unused-vars */
 import './NewPost.css'
 
+import blogFetch from '../axios/config.js'
+
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 const NewPost = () => {
+  const navigate = useNavigate()
+
+  const [title, setTitle] = useState()
+  const [body, setBody] = useState()
+
+  const createPost = async (e) => {
+    e.preventDefault();
+
+    const post = { title, body, userId: 1 }
+
+    await blogFetch.post('/posts', {
+      body: post,
+    })
+
+    navigate('/')
+
+  }
+
   return (
     <div className="new-post">
       <h2>Insert new post</h2>
-      <form>
+      <form onSubmit={(e) => createPost(e)}>
         <div className="form-control">
           <label htmlFor="title">Title:</label>
           <input type="text"
-          name="title"
-          id="title"
-          placeholder="Type the title" />
+            name="title"
+            id="title"
+            placeholder="Type the title"
+            onChange={(e) => setTitle(e.target.value)} />
         </div>
         <div className="form-control">
           <label htmlFor="body">Content:</label>
-          <textarea 
-          name="body"
-          id="body"
-          placeholder="Type the content" ></textarea>
+          <textarea
+            name="body"
+            id="body"
+            placeholder="Type the content"
+            onChange={(e) => setBody(e.target.value)} ></textarea>
         </div>
         <input
-         type="submit"
-         value='Create Post'
-         className="btn" />
+          type="submit"
+          value='Create Post'
+          className="btn" />
       </form>
     </div>
   )
