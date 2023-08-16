b1c6704c31f40edca1147f0011e6d0570a127471
Lucas Telles <lltellesss@gmail.com>
2023-08-16 17:51:01 -0300

    axios posts read/home css

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -17,7 +17,8 @@ const Home = () => {
       const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
 
       const data = response.data
-      console.log(data)
+
+      setPosts(data)
 
     } catch (error) {
       console.log(error)
@@ -32,7 +33,18 @@ const Home = () => {
   }, [])
 
   return (
-    <div>Home</div>
+    <div className="home">
+        <h1>Last Posts</h1>
+        {posts.length	=== 0 ? <p>Carregando...</p> : (
+          posts.map((post) => (
+            <div className="post" key={post.id}>
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+              <Link to={`/post/${post.id}`} className="btn">Read more</Link>
+            </div>
+          ))
+        )}
+    </div>
   )
 }
 
